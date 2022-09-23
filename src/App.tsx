e0d8d0ba1d99df82a34de24db06eff459e5656dd
Ahmad Sandid <ahmad1702@gmail.com>
2022-09-23 01:13:49 -0500

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+type StateType = "neutral" | "incorrect" | "correct";
+type LetterBoxProps = {
+  key?: string | number;
+  char: string;
+  state: StateType;
+};
+const LetterBox = ({ char }: LetterBoxProps) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-16 w-16 bg-slate-800 rounded-2xl mr-2 flex items-center justify-center text-4xl text-white font-bold pb-1">
+      {char}
     </div>
   );
-}
+};
+
+const App = () => {
+  return (
+    <div className="h-screen overflow-hidden flex flex-col items-center justify-center">
+      {[0, 1, 2, 3, 4, 5].map((i: number) => (
+        <div key={i} className="flex mb-2">
+          {[0, 1, 2, 3, 4].map((j: number) => (
+            <LetterBox key={j} char={"A"} state={"neutral"} />
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default App;
